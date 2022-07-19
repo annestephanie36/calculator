2bdf78e231a0286baffd9a77fcaeb1aad3f7600c
annestephanie36 <stephyannah36@gmail.com>
2022-07-19 10:00:59 +0300

    modified calculator

---
+++ calculator.py
@@ -0,0 +1,36 @@
+number_1=int(input('Enter your first number:'))
+number_2=int(input('Enter your second number:'))
+
+number_1=int(input('Enter your first number:'))
+number_2=int(input('Enter your second number:'))
+print('number_1 + number_2')
+
+number_1=int(input('Enter your first number:'))
+number_2=int(input('Enter your second number:'))
+print('{} + {}='.format(number_1 + number_2))
+print(number_1 + number_2)
+
+number_1=int(input('Enter your first number:'))
+number_2=int(input('Enter your seond number:'))
+
+PycharmProjects/pythonProject1/main.py
+
+#Addition
+print('{} + {}='.format(number_1,number_2))
+print(number_1 + number_2)
+
+
+#subtraction
+print('{} - {}='.format(number_1,number_2))
+print(number_1 - number_2)
+
+
+#multiplication
+print('{} * {}='.format(number_1,number_2))
+print(number_1 * number_2)
+
+
+#division
+print('{} / {}='.format(number_1,number_2))
+print(number_1 / number_2)
+
